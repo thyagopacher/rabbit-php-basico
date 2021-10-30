cf44ecf8a3a17a07683f7a52dee29c669aab4058
thyago henrique pacher <thyago.pacher@gmail.com>
2021-10-30 11:32:37 -0300

    projeto base rabbit

---
+++ send.php
@@ -0,0 +1,17 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$connection = new AMQPStreamConnection('rabbit', 5672, 'admin', 'admin');
+$channel = $connection->channel();
+
+$channel->queue_declare('hello', false, false, false, false);
+
+$msg = new AMQPMessage('Hello World!');
+$channel->basic_publish($msg, '', 'hello');
+
+echo " [x] Sent 'Hello World!'\n";
+
+$channel->close();
+$connection->close();
